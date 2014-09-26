6e2e89b2dc5caf08e644c90f7983389e6ec83ca4
Nikesh Srivastava <nikesh.srivastava@outlook.com>
2014-09-25 20:56:16 -0700

    Added tests for boolean logic

--- test/Test.hs
+++ test/Test.hs
@@ -15,6 +15,16 @@ tests =
     , ("S S S S -> S S (S S)", evaluate (Apply (Apply ss S) S) == (Apply ss ss))
     , ("K (K K K) K -> K", evaluate (Apply (Apply K (Apply kk K)) K) == K)
     , ("K K omega -> K", evaluate (Apply kk omega) == K)
+    , ("T NOT -> F -> S K", evaluate (not t) == f)
+    , ("F NOT -> T -> K", evaluate (not f) == t)
+    , ("T OR T -> T -> K", evaluate (or t t) == t)
+    , ("T OR F -> T -> K", evaluate (or t f) == t)
+    , ("F OR T -> T -> K" , evaluate (or f t) == t)
+    , ("F OR F -> F -> S K", evaluate (or f f) == f)
+    , ("T T AND -> T -> K", evaluate (and t t) == t)
+    , ("T F AND -> F -> S K", evaluate (and t f) == f)
+    , ("F T AND -> F -> S K", evaluate (and f t) == f)
+    , ("F F AND -> F -> S K", evaluate (and f f) == f)
     ]
     where
     kk = Apply K K
@@ -24,3 +34,8 @@ tests =
     i = Apply sk K
     sii = Apply (Apply S i) i
     omega = Apply sii sii
+    t = K
+    f = sk
+    not x = Apply (Apply x f) t
+    or a b = Apply (Apply a t) b
+    and a b = Apply (Apply a b) f
